3f9194525c114c660f62167e6376e3f7ccf722f4
Jason Alls <jason.alls@gl-education.com>
2020-08-16 21:49:47 +0100

    Added Chapter 1 projects.

---
+++ CH01/CH01_Books/CH01_Books/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using CH01_Books;
+
+var darkSideOfTheMoon = new Book { Title = "Dark Side of The Moon", Author = "Arthur C. Clarke" };
+
+Console.WriteLine($"{darkSideOfTheMoon.Title} is written by {darkSideOfTheMoon.Author}. Well worth reading!");
---
+++ CH01/CH01_Records/CH01_Records/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using CH01_Records;
+
+var theOtherSideOfTheSky = new Book { 
+    Title = "The Other Side of The Sky", 
+    Author = "Arthur C. Clarke",
+    PublisherName = "Victor Gollancz Ltd."
+};
+
+var redezvousWithRama = theOtherSideOfTheSky with { 
+    Title = "Rendezvous with Rama"
+};
+
+var ramaII = redezvousWithRama with { 
+    Title = "Rama II"
+};
+
+var theGardenOfRama = ramaII with { 
+    Title = "The Garden of Rama",
+};
+
+var ramaRevealed = theGardenOfRama with { 
+    Title = "Rama Revealed"
+};
+
+var theHammerOfGod = ramaRevealed with { 
+    Title = "The Hammer of God"
+};
+
+Console.WriteLine($"Some of {ramaII.Author}'s books include:\n");
+Console.WriteLine($"- {theOtherSideOfTheSky.Title}");
+Console.WriteLine($"- {redezvousWithRama.Title}");
+Console.WriteLine($"- {ramaII.Title}");
+Console.WriteLine($"- {theGardenOfRama.Title}");
+Console.WriteLine($"- {ramaRevealed.Title}");
+Console.WriteLine($"- {theHammerOfGod.Title}");
+
+Console.WriteLine($"\nMy favourite book by {theOtherSideOfTheSky.Author} is {theOtherSideOfTheSky.Title}.");
+Console.WriteLine($"These books were originally published by {theHammerOfGod.PublisherName}.");
+
+var book = ramaII with { Title = "The Other Side of The Sky" };
+var booksEqual = Object.Equals(book, theOtherSideOfTheSky) ? "Yes" : "No";
+Console.WriteLine($"Are {book.Title} and {theOtherSideOfTheSky.Title} equal? {booksEqual}");
+
+var intelliJ = new Product("JetBrains Intelli-J", "Advanced Java Code IDE");
+var (product, description) = intelliJ;
+
+Console.WriteLine($"The product called {product} is an {description}.");
