14c50137c4006d041128fa1353e15eaa33ab1dea
Jason Alls <jason.alls@gl-education.com>
2020-09-02 22:21:07 +0100

    Added Chapter 2 source code.

---
+++ CH02/CH02_Excel/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CH02_Excel
+{
+    using System;
+    using Excel = Microsoft.Office.Interop.Excel;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var excel = new Excel.Application();
+            var workbook = excel.Workbooks.Open("C:\\Temp\\mysheet.xlsx");
+            var worksheet = excel.ActiveSheet as Excel.Worksheet;
+            
+            Excel.Range userRange = worksheet.UsedRange;
+            int countRecords = userRange.Rows.Count;
+            int add = countRecords + 1;
+            worksheet.Cells[add, 1] = $"Total Rows: {countRecords}";
+
+            workbook.Close(true, Type.Missing, Type.Missing);
+            excel.Quit();
+        }
+    }
+}
---
+++ CH02/CH02_UnsafeCode/Integers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH02_UnsafeCode
+{
+    internal struct Integers
+    {
+        public int I, J;
+    }
+}
