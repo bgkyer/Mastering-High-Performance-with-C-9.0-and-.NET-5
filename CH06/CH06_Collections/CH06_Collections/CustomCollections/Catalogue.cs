9184323339248f9be7c541b5f140c3f2243827d1
Jason Alls <jason.alls@gl-education.com>
2021-02-14 21:05:33 +0000

    Added missing code including a custom collection.

--- CH06/CH06_Collections/CH06_Collections/CustomCollections/Catalogue.cs
+++ CH06/CH06_Collections/CH06_Collections/CustomCollections/Catalogue.cs
@@ -6,23 +6,41 @@ using System.Threading.Tasks;
 
 namespace CH06_Collections.CustomCollections
 {
-	using CH06_Collections.CustomCollections;
-	using CH06_Collections.Models;
-	using System.Collections;
 
 	using BenchmarkDotNet.Attributes;
 	using BenchmarkDotNet.Engines;
 	using BenchmarkDotNet.Order;
+
+	using CH06_Collections.CustomCollections;
 	using CH06_Collections.Linq;
+	using CH06_Collections.Models;
+
+	using System.Collections;
 	using System.Collections.Generic;
 	using System.Threading;
 
 	[MemoryDiagnoser]
 	[Orderer(SummaryOrderPolicy.FastestToSlowest)]
 	[RankColumn]
-	public class Catalogue
+	public class Catalogue : ICollection
 	{
-		private Product[] Products = Array.Empty<Product>();
+		private Product[] Products;
+
+		public int Count => Products.Length;
+
+		public bool IsSynchronized => false;
+
+		public object SyncRoot => this;
+
+		public Catalogue()
+		{
+			Products = Array.Empty<Product>();
+		}
+
+		public Catalogue(Product[] products)
+		{
+			this.Products = products;
+		}
 
 		[Benchmark]
 		public void Add(Product product)
@@ -59,6 +77,15 @@ namespace CH06_Collections.CustomCollections
 		public void Remove(int index)
 		{
 			Products = Products.Remove<Product>(index);
+		}
+
+		public void CopyTo(Array array, int index)
+		{
+			foreach(Product product in Products)
+			{
+				array.SetValue(product, index);
+				index++;
+			}
 		}
 	}
 }
--- CH06/CH06_Collections/CH06_Collections/Linq/LinqPerformance.cs
+++ CH06/CH06_Collections/CH06_Collections/Linq/LinqPerformance.cs
@@ -43,6 +43,71 @@
 		}
 
 		[Benchmark]
+		public List<Person> FilterGroupsVersion1()
+		{
+			return (from p in _people
+					where _group1.Contains(p.LastName.ToLower())
+					 || _group2.Contains(p.LastName.ToLower())
+					select p).ToList();
+		}
+
+		[Benchmark]
+		public List<Person> FilterGroupsVersion2()
+		{
+			return (from p in _people
+					let lastName = p.LastName.ToLower()
+					where _group1.Contains(lastName)
+					|| _group2.Contains(lastName)
+					select p).ToList();
+		}
+
+		[Benchmark]
+		public List<Person> FilterGroupsVersion3()
+		{
+			List<Person> people = new List<Person>();
+			for (int i = 0; i < _people.Count; i++)
+			{
+				var person = _people[i];
+				var lastName = person.LastName.ToLower();
+				if (
+					_group1.Contains(lastName)
+					|| _group2.Contains(lastName)
+				)
+					people.Add(person);
+			}
+			return people;
+		}
+
+		[Benchmark]
+		public List<Person> FilterGroupsVersion4()
+		{
+			List<Person> people = new List<Person>();
+			for (int i = 0; i < _people.Count; i++)
+			{
+				var person = _people[i];
+				var lastName = person.LastName.ToLower();
+				if (
+					_group2.Contains(lastName)
+					|| _group1.Contains(lastName)
+				)
+					people.Add(person);
+			}
+			return people;
+		}
+
+		[Benchmark]
+		public Person GetLastPersonVersion1()
+		{
+			return _people.Last();
+		}
+
+		[Benchmark]
+		public Person GetLastPersonVersion2()
+		{
+			return _people[_people.Count - 1];
+		}
+
+		[Benchmark]
 		public List<Person> GroupByVersion1()
 		{
 			return _people
